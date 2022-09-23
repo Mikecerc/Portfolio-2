44d0a77b4884ca9545809408dec35c61cc266535
Mikecerc1 <mikecerc1@outlook.com>
2022-09-22 23:49:59 -0400

    init

---
+++ components/base/header.vue
@@ -0,0 +1,33 @@
+<template>
+    <div id="header-container">
+        <div class="logo"></div>
+        <nav class="navbar">
+            <div class="navbar-links">
+                <ul>
+                    <li>
+                        <div><nuxtLink to="/index.html">Home</nuxtLink></div>
+                    </li>
+                    <li>
+                        <div><nuxtLink to="/projects.html">Projects</nuxtLink></div>
+                    </li>
+                    <li>
+                        <div class="dropdown">
+                            <button class="dropbtn">Documentation<i class="fa fa-caret-down"></i></button>
+                            <div class="dropdown-content">
+                                <div><nuxtLink to="/docs/batteryLog.html">Digital Battery Logging (frc)</nuxtLink></div>
+                                <div><nuxtLink to="/docs/batteryMonitoring.html">Automated Battery Monitoring</nuxtLink></div>
+                                <div><nuxtLink to="/docs/discordBots.html">Discord Bots</nuxtLink></div>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div><nuxtLink to="/aboutMe.html">about Me</nuxtLink></div>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</template>
+<style>
+@import "../../assets/css/build/layout/header.css";
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="page-container">
+        <div class="page-background">
+            <div class="content">
+                <div class="header">
+                    <BaseHeader />
+                </div>
+                <div class="content">
+                    <slot />
+                </div>
+                <div class="footer">
+                    <BaseFooter />
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<style></style>
