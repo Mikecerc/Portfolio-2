36a5490de13c9152e55ed9eed01ed0a292fe9fae
Mikecerc1 <mikecerc1@outlook.com>
2022-09-25 14:20:59 -0400

    added toggle dropdown to mobile design

--- components/base/header.vue
+++ components/base/header.vue
@@ -28,33 +28,32 @@
             </div>
         </nav>
         <nav class="navbar-mobile">
-            <button class="mobile-main-dropdown" @click="showMenu()">
-                <i v-show="(menuOut == false)" class="fa fa-bars"></i>
-                <i v-show="(menuOut == true)" class="fa fa-times"></i>
-            </button>
-            <div v-show="(menuOut == true)" class="mobile-navbar-container">
-                <ul>
-                    <li>
-                        <div><nuxtLink @click="showMenu()" class="link" to="/">Home</nuxtLink></div>
-                    </li>
-                    <li>
-                        <div><nuxtLink @click="showMenu()" class="link" to="/projects">Projects</nuxtLink></div>
-                    </li>
-                    <li>
-                        <div class="dropdown">
-                            <button class="dropbtn">Documentation<i class="fa fa-caret-down"></i></button>
-                            <div class="dropdown-content">
-                                <div><nuxtLink to="/docs/batteryLog" @click="showMenu()">Digital Battery Logging (frc)</nuxtLink></div>
-                                <div><nuxtLink to="/docs/batteryMonitoring" @click="showMenu()">Automated Battery Monitoring</nuxtLink></div>
-                                <div><nuxtLink to="/docs/discordBots" @click="showMenu()">Discord Bots</nuxtLink></div>
-                            </div>
+            <ul v-show="menuOut == true" class="mobile-navbar-container">
+                <li>
+                    <div><nuxtLink @click="showMenu()" class="link" to="/">Home</nuxtLink></div>
+                </li>
+                <li>
+                    <div><nuxtLink @click="showMenu()" class="link" to="/projects">Projects</nuxtLink></div>
+                </li>
+                <li>
+                    <div class="dropdown">
+                        <button class="dropbtn" @click="openDropdown()">Documentation<i class="fa fa-caret-down"></i></button>
+                        <div class="dropdown-content" v-show="dropOut == true">
+                            <div><nuxtLink to="/docs/batteryLog" @click="showMenu()">Digital Battery Logging (frc)</nuxtLink></div>
+                            <div><nuxtLink to="/docs/batteryMonitoring" @click="showMenu()">Automated Battery Monitoring</nuxtLink></div>
+                            <div><nuxtLink to="/docs/discordBots" @click="showMenu()">Discord Bots</nuxtLink></div>
                         </div>
-                    </li>
-                    <li>
-                        <div><nuxtLink class="link" to="/aboutMe" @click="showMenu()">about Me</nuxtLink></div>
-                    </li>
-                </ul>
-            </div>
+                    </div>
+                </li>
+                <li>
+                    <div><nuxtLink class="link" to="/aboutMe" @click="showMenu()">about Me</nuxtLink></div>
+                </li>
+            </ul>
+
+            <button class="mobile-main-dropdown" @click="showMenu()">
+                <i v-show="menuOut == false" class="fa fa-bars"></i>
+                <i v-show="menuOut == true" class="fa fa-times"></i>
+            </button>
         </nav>
     </div>
 </template>
@@ -64,7 +63,11 @@
 <script setup lang="ts">
 import { Ref } from "vue";
 let menuOut: Ref<boolean> = ref(false);
+let dropOut: Ref<boolean> = ref(false);
 function showMenu(): void {
     menuOut.value = !menuOut.value;
+}
+function openDropdown() {   
+    dropOut.value = !dropOut.value;
 }
 </script>
