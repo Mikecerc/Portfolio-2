f3799f1e589d8dc1f78374c253a69534442b42f4
Mikecerc1 <Mikecerc1@outlook.com>
2022-10-04 12:38:09 -0400

    resume + contact

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,8 +1,7 @@
 <template>
-    <div><ViewsConstruction /></div>
+    <div>
+      <div class="card">Please feel free to contact me at michael@mikecerc.com with any questions or professional inquiries.</div>
+    </div>
 </template>
 <script setup>
-definePageMeta({
-  layout: 'construction'
-})
 </script>
--- pages/resume.vue
+++ pages/resume.vue
@@ -1,8 +1,10 @@
 <template>
-    <div><ViewsConstruction /></div>
+    <div>
+      <div class="card">
+        <h1>Resume</h1>
+        <p>View my resume <a href="/documents/resume-websiteVersion.pdf">here</a>! Please do not hesitate to reach out to me if you have any questions</p>
+      </div>
+    </div>
 </template>
 <script setup>
-definePageMeta({
-  layout: 'construction'
-})
 </script>
